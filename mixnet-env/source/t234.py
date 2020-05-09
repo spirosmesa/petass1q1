2a415b87e36bf19acdb53f353d5f747615bdb41b
john doe <spirosmesa@yahoo.gr>
2020-05-09 17:17:50 +0200

    cleanup

--- mixnet-env/source/t234.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-
-def dte(a,b):
-	print(a)
-	print(str(argv[1]).encode("UTF-8"))
-
-
-
-dte(argv[0], argv[1])
--- mixnet-env/source/test.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-out=$((ls ./logs | grep entry > out.txt) && (wc -l out.txt | grep -Eo '[[:digit:]]{2}' | tr -d "\n"))
-
-echo $out
