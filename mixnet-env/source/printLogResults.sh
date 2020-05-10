09538b1f7d2ab03daaad178e5de1d27b7c4af497
john doe <spirosmesa@yahoo.gr>
2020-05-10 14:43:27 +0200

    final push

--- mixnet-env/source/printLogResults.sh
+++ mixnet-env/source/printLogResults.sh
@@ -2,10 +2,16 @@
 
 outputCount=$( (ls ./$1 | grep entry > out.txt) && (wc -l out.txt | grep -Eo '[[:digit:]]{4}') )
 
-for i in $(seq 1 60); do
+for i in $(seq 1 100); do
 	entry=$(wc -l ./$1/entry$i.txt | sed 's/\s.*$//') 
 	exite=$(wc -l ./$1/exit$i.txt | sed 's/\s.*$//')
 	echo "entry message count $i: "  "$(($entry-1))" 
+	#echo "entry log is:"
+	#cat ./$1/entry$i.txt
+	echo ""
 	echo "exit message count $i: " "$(($exite-1))" 
+	echo "exit log is:"
+	cat ./$1/exit$i.txt
+	echo ""
 	echo ""
 done
